cd61ea2a68b79fda3444c9c74ffdeb7978b5a014
shishiqiqiu <2995818289@qq.com>
2025-11-20 10:49:18 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex-center h-[100vh]">
+    <h1 class="text-3xl text-indigo-300">Hello, GSAP!</h1>
+  </div>
+</template>
+
+<script setup>
+import { gsap } from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+gsap.registerPlugin(ScrollTrigger, SplitText);
+</script>
