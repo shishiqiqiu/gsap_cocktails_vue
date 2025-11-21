1fcd63900797cc983577d4f96c70e176e272d121
shishiqiqiu <2995818289@qq.com>
2025-11-21 12:10:58 +0800

    add Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
-  <div class="flex-center h-[100vh]">
-    <h1 class="text-3xl text-indigo-300">Hello, GSAP!</h1>
-  </div>
+  <main>
+    <Navbar />
+  </main>
 </template>
 
 <script setup>
 import { gsap } from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
+
+import Navbar from "./components/Navbar.vue";
+
 gsap.registerPlugin(ScrollTrigger, SplitText);
 </script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,41 @@
+<template>
+  <nav>
+    <div>
+      <a href="#home" class="flex items-center gap-2">
+        <img src="/images/logo.png" alt="logo" />
+        <p>Velvet Pour</p>
+      </a>
+      <ul>
+        <li v-for="link in navLinks" :key="link.id">
+          <a :href="link.id">{{ link.title }}</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+import { onMounted } from "vue";
+import { gsap } from "gsap";
+import { navLinks } from "../../constants";
+
+onMounted(() => {
+  const navTween = gsap.timeline({
+    scrollTrigger: {
+      trigger: "nav",
+      start: "bottom top",
+    },
+  });
+
+  navTween.fromTo(
+    "nav",
+    { backgroundColor: "transparent" },
+    {
+      backgroundColor: "#00000050",
+      backgroundFilter: "blur(10px)",
+      duration: 1,
+      ease: "power1.inOut",
+    }
+  );
+});
+</script>
