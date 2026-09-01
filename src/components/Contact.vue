646525baee4417902826d35c5689b7aef6baf3cb
shishiqiqiu <2995818289@qq.com>
2026-09-01 23:47:53 +0800

    change contact info

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -10,12 +10,12 @@
       <h2>Where to Find Us</h2>
       <div>
         <h3>Visit Our Bar</h3>
-        <p>456, Raq Blvd. #404, Los Angeles, CA 90210</p>
+        <p>123, Raq Blvd. 456, Los Angeles</p>
       </div>
       <div>
         <h3>Contact Us</h3>
-        <p>(555) 987-6543</p>
-        <p>hello@jsmcocktail.com</p>
+        <p>(12) 345-6789</p>
+        <p>zyb@exmaple.com</p>
       </div>
 
       <div>
